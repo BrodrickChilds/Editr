7a0b14ce3df5288aaf3836b04a8ef231b185382c
Brodrick <brodrick.childs@gmail.com>
2012-04-10 01:03:52 -0400

    made blog scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Editr::Application.config.secret_token = '0e7b7e5d195b2cefa6ba8d02ebdc5e01af58ae795107f2b386e0756be24232b4498951e79d33ea180d045ad2a1af7c503694bbddd9fbdbe5066aa9665d3636d2'
